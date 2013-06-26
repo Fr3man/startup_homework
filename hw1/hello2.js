821075c5e3fc228683973b4807a0f24d95e0d52e
fr3man <konovalov87@gmail.com>
2013-06-26 16:13:16 +0400

    Project structure reogranization

--- hw1/hello2.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-var fs = require('fs');
-var outfile = "hello.txt";
-var out = "I'm a cool node.js developer.\n";
-fs.writeFileSync(outfile, out);
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
